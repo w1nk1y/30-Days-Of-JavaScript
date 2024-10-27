9e345fdc1d65fa56195c84e6399f7e452bc20310
w1nk1y <mil.nemo@mail.ru>
2024-10-27 14:26:49 +0600

    [+] Add Day-1

---
+++ 01_Day_Introduction/variable.js
@@ -0,0 +1,17 @@
+// Declaring different variables of different data types
+let firstName = 'Andrey' // first name of a person
+let lastName = 'Miller' // last name of a person
+let country = 'Russia' // country
+let city = 'Omsk' // capital city
+let age = 16 // age in years
+let isMarried = false
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Andrei', //name of a person
+  job = 'student',
+  live = 'Russia'
